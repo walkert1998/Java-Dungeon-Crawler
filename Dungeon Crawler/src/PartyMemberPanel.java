d5013dc293d5e81b46ef818ed80c307faaef75df
Thomas Walker <twalker.mhd@gmail.com>
2019-05-31 22:09:19 -0300

    Removed test file and added action listeners to hand buttons

--- Dungeon Crawler/src/PartyMemberPanel.java
+++ Dungeon Crawler/src/PartyMemberPanel.java
@@ -3,6 +3,8 @@ import javax.swing.JButton;
 import javax.swing.JProgressBar;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PartyMemberPanel extends JPanel {
 
@@ -14,33 +16,52 @@ public class PartyMemberPanel extends JPanel {
         setLayout(null);
         
         JButton btnFace = new JButton("");
+        JProgressBar HealthBar = new JProgressBar();
+        JProgressBar ManaBar = new JProgressBar();
+        JButton btnLeftHand = new JButton("");
+        JButton btnRightHand = new JButton("");
+        JButton btnX = new JButton("X");
+        
+        
         btnFace.setIcon(new ImageIcon("C:\\Users\\Tommy\\workspace\\Dungeon Crawler\\Dantes.gif"));
         btnFace.setBounds(0, 25, 82, 82);
         add(btnFace);
         
-        JProgressBar HealthBar = new JProgressBar();
+        
         HealthBar.setValue(85);
         HealthBar.setForeground(new Color(255, 0, 0));
         HealthBar.setBounds(81, 25, 111, 41);
         add(HealthBar);
         
-        JProgressBar ManaBar = new JProgressBar();
+        
         ManaBar.setValue(75);
         ManaBar.setForeground(new Color(0, 191, 255));
         ManaBar.setBounds(81, 66, 111, 41);
         add(ManaBar);
         
-        JButton btnLeftHand = new JButton("");
+        
+        btnLeftHand.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent arg0)
+            {
+                
+            }
+        });
         btnLeftHand.setIcon(new ImageIcon("C:\\Users\\Tommy\\workspace\\Dungeon Master Java\\Items\\firestaff.gif"));
         btnLeftHand.setBounds(0, 107, 96, 83);
         add(btnLeftHand);
         
-        JButton btnRightHand = new JButton("");
+        btnRightHand.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                
+            }
+        });
         btnRightHand.setIcon(new ImageIcon("C:\\Users\\Tommy\\workspace\\Dungeon Master Java\\Items\\bolt_blade.gif"));
         btnRightHand.setBounds(96, 107, 96, 83);
         add(btnRightHand);
         
-        JButton btnX = new JButton("X");
         btnX.setBounds(132, 0, 60, 23);
         add(btnX);
 
